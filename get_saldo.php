a35449922ffca518f7905d65ff7218605f77ca5f
Sofyan Setiadi <sofinranger@gmail.com>
2023-11-15 16:12:29 +0700

    fix:query

--- get_saldo.php
+++ get_saldo.php
@@ -18,7 +18,11 @@
     }
     
     $tanggal = $tglsystem;
-    $sql = "SELECT b.asal_divisi,b.kode_jenis_bahan,SUM(b.stock_akhir) AS stock_akhir,SUM(b.berat_akhir) AS berat_akhir,(sum(b.tambahan)+sum(b.batu_admin2)) as tambahan FROM tt_stock_card a INNER JOIN tt_po_job_order b ON (b.no_job_order = a.no_job_order AND b.kode_barang = a.kode_barang AND a.divisi=b.asal_divisi AND b.update_date = a.input_date) WHERE b.asal_divisi=b.tujuan_divisi AND a.status='OPEN' AND b.status_job NOT IN ('DONE','CANC','GB') AND b.asal_divisi IN ('FILLING','FR','FR2','FR3','HANDSETTING1','HANDSETTING2','POLISHING','PLATTING') AND a.type NOT IN ('N') GROUP BY b.asal_divisi,b.kode_jenis_bahan;";
+
+    $delete = "DELETE FROM tt_saldo_outstand_divisi WHERE tanggal='$tanggal'";
+    $conn->query($delete);
+
+    $sql = "SELECT b.asal_divisi,b.kode_jenis_bahan,SUM(b.stock_akhir) AS stock_akhir,SUM(b.berat_akhir) AS berat_akhir,(sum(b.tambahan)+sum(b.batu_admin2)) as tambahan FROM tt_stock_card a INNER JOIN tt_po_job_order b ON (b.no_job_order = a.no_job_order AND b.kode_barang = a.kode_barang AND a.divisi=b.asal_divisi AND b.update_date = a.input_date) WHERE b.asal_divisi=b.tujuan_divisi AND a.status='OPEN' AND b.status_job NOT IN ('DONE','CANC','GB') AND b.asal_divisi IN ('POTONG SPRU','FILLING','FR','FR2','FR3','HANDSETTING1','HANDSETTING2','POLISHING','PLATTING') AND a.type NOT IN ('N') GROUP BY b.asal_divisi,b.kode_jenis_bahan;";
     if ($conn){
     }else{
         $data['pesan'] = "gagal ".mysqli_connect_error();
@@ -37,7 +41,7 @@
             $berat_akhir = number_format((float)$row["berat_akhir"], 0, '.', '');
             $tambahan = number_format((float)$row["tambahan"], 3, '.', '');
             
-            $insert = "INSERT INTO tt_saldo_outstand_divisi (tanggal,kode_jenis_bahan,divisi,total_stock,total_berat,total_tambahan,input_date) VALUES ('$tanggal','$divisi','$kode_jenis_bahan','$stock_akhir','$berat_akhir','$tambahan','$inputDate')";
+            $insert = "INSERT INTO tt_saldo_outstand_divisi (tanggal,divisi,kode_jenis_bahan,total_stock,total_berat,total_tambahan,input_date) VALUES ('$tanggal','$divisi','$kode_jenis_bahan','$stock_akhir','$berat_akhir','$tambahan','$inputDate')";
             $conn->query($insert);
         }
 
